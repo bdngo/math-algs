cfaf7b20910114b17b8ad6316e9d2612b9ec9d8d
bdngo <bryanngo@berkeley.edu>
2020-01-05 18:02:43 -0800

    added perfect_num and factors functions

--- factors.py
+++ factors.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 def prime_factors(n, k=2):
     """Returns the prime factorization of a natural number N."""
     if n == 1:
@@ -6,16 +8,30 @@ def prime_factors(n, k=2):
         return [k] + prime_factors(n // k, k)
     return prime_factors(n, k + 1)
 
-def print_pf(factors):
-    """Prints the prime factorization from a list of factors."""
+def print_pf(n):
+    """Returns the (nicer) prime factorization of a natural number N."""
     factors_dict = {}
-    for i in factors:
+    for i in prime_factors(n):
         if i not in factors_dict:
             factors_dict[i] = 1
         else:
             factors_dict[i] += 1
     factors_str = ''
-    for p_factor, power in factors_dict.items():
-        factors_str += '{0}^{1} * '.format(p_factor, power)
+    for prime, power in factors_dict.items():
+        factors_str += '{0}^{1} * '.format(prime, power)
     return factors_str[:-3]
+
+def factors(n):
+    """Returns all the factors of an integer N."""
+    factors = []
+    for i in range(1, n + 1):
+        if n % i == 0:
+            factors += [i]
+    return factors
+
+def perfect_num(n):
+    """Determines if natural number N is a perfect number."""
+    if sum(factors(n)[:-1]) == n:
+        return True
+    return False
 
