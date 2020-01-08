d17248eefd6c63b7eba292f5be94a947cbf0b454
bdngo <bryanngo@berkeley.edu>
2020-01-08 11:04:15 -0800

    added monte_carlo

---
+++ monte_carlo.py
@@ -0,0 +1,20 @@
+from random import uniform
+import math as m
+
+def estimate_pi():
+    """Approximates π."""
+    samples, in_func = 1000, 0
+    for _ in range(samples):
+        rand_x, rand_y = uniform(-1, 1), uniform(-1, 1)
+        if rand_x**2 + rand_y**2 <= 1:
+            in_func += 1
+    return 4 * (in_func / samples)
+
+def monte_carlo_integration(f, bounds):
+    left, right = bounds[0], bounds[1]
+    samples, integral = 1000000, 0.
+    for _ in range(samples):
+        rand_point = uniform(left, right)
+        integral += f(rand_point)
+    return (right - left) / samples * integral
+
