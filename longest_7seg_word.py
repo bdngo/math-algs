fda4affe5a9d2853584604bd989628c153836fd5
bdngo <bryanngo@berkeley.edu>
2020-01-10 17:26:49 -0800

    added longest_7seg_word.py

---
+++ longest_7seg_word.py
@@ -0,0 +1,13 @@
+from re import search
+
+bad_letters = '[gkmqvwxz]'
+words_list = open("words.txt").read().split('\n')
+
+def longest_word(txt, regex):
+    """Returns the longest string in TXT excluding REGEX."""
+    longest = ''
+    for i in txt:
+        if len(i) > len(longest) and not search(bad_letters, i):
+            longest = i
+    return longest
+
