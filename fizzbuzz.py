c5ad037e134959c96b3b722cdfa2cb1259609797
bdngo <bryanngo@berkeley.edu>
2020-01-12 13:46:39 -0800

    organized into folders, updated README.md

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,18 @@
 def fizzbuzz(n):
-    """Solves the FizzBuzz problem."""
+    """Solves the FizzBuzz problem.
+
+    >>> fizzbuzz(10)
+    1
+    2
+    fizz
+    4
+    buzz
+    6
+    7
+    8
+    fizz
+    buzz
+    """
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
             print('fizzbuzz')
@@ -9,6 +22,4 @@ def fizzbuzz(n):
             print('buzz')
         else:
             print(i)
-    return
 
-fizzbuzz(100)
