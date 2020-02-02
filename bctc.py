3030d971ebff0f8f18bf5edeb099686d7f378b5e
Bryan Ngo <bryanngo@berkeley.edu>
2020-02-02 00:49:37 -0800

    added bctc

---
+++ bctc.py
@@ -0,0 +1,11 @@
+from random import randint
+
+def bctc(rolls):
+    """Calculates the win/loss ratio for the Vietnamese betting game 'Bầu Cua Tôm Cá.'"""
+    wins = 0
+    for _ in range(rolls):
+        dice, bet = [randint(1, 6) for _ in range(3)], randint(1, 6)
+        if dice.count(bet):
+            wins += 1
+    return wins / rolls * 100
+
