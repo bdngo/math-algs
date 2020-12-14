09e310abf458c5ba805d7c74c4ddb3e07baa11f2
Bryan Ngo <bryanngo@berkeley.edu>
2020-12-14 13:43:12 -0800

    turned dfs and bfs into proper functions

--- data-structures/graph.py
+++ data-structures/graph.py
@@ -18,30 +18,30 @@ class Graph(Generic[T]):
     def __repr__(self) -> str:
         return self.val
 
-    def dfs(self, start: Graph, visit: Callable[[Graph], Any]) -> List[Graph]:
-        """
-        Performs depth-first search on this graph with start node START.
-        """
-        fringe, marked = [], []
-        fringe.append(start)
-        while fringe != []:
-            v = fringe.pop()
-            if v not in marked:
-                marked.append(v)
-                visit(v)
-                fringe.extend(i for i in v.nodes if i not in marked)
-        return marked
-    
-    def bfs(self, start: Graph, visit: Callable[[Graph], Any]) -> List[Graph]:
-        """
-        Performs breadth-first search on this graph with start node START.
-        """
-        fringe, marked = deque([]), []
-        fringe.append(start)
-        while fringe != []:
-            v = fringe.pop()
-            if v not in marked:
-                marked.append(v)
-                visit(v)
-                fringe.extendleft(i for i in v.nodes if i not in marked)
-        return marked
+def dfs(start: Graph, visit: Callable[[Graph], Any]) -> List[Graph]:
+    """
+    Performs depth-first search on this graph with start node START.
+    """
+    fringe, marked = [], []
+    fringe.append(start)
+    while fringe != []:
+        v = fringe.pop()
+        if v not in marked:
+            marked.append(v)
+            visit(v)
+            fringe.extend(i for i in v.nodes if i not in marked)
+    return marked
+
+def bfs(start: Graph, visit: Callable[[Graph], Any]) -> List[Graph]:
+    """
+    Performs breadth-first search on this graph with start node START.
+    """
+    fringe, marked = deque([]), []
+    fringe.append(start)
+    while fringe != []:
+        v = fringe.pop()
+        if v not in marked:
+            marked.append(v)
+            visit(v)
+            fringe.extendleft(i for i in v.nodes if i not in marked)
+    return marked
